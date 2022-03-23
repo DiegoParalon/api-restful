3c5250f9bb7b38bfba94ac22027735edc02012f6
Diego Paralon <diegoparalon@hotmail.com>
2022-03-23 18:05:55 +0000

    estrutura inicial do projeto

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+// const path = require('path')
+
+// const db = require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+//conexao com o banco de dados
+// db.connect()
+
+
+// habilita server para receber dados via post (formulario)
+app.use(express.urlencoded({ extended: true}))
+
+//definindo as rotas
+app.use('/api', routes)
+
+// executando o servidor 
+const port = process.env.PORT || 9090
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
+
